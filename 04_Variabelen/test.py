77067f460a7cd4c71f05b1f1a4e6e424d6018b43
ArthurCallewaert <arthur.callewaert@sgsintpaulus.eu>
2019-09-20 11:24:56 +0200

    patroontje

---
+++ 04_Variabelen/test.py
@@ -0,0 +1,15 @@
+# wisselen van waarden
+# x, y = 2, 'hallo'
+# x, y = y, x
+# print(x, y)
+
+a = float(input('a: '))
+b = float(input('b: '))
+c = float(input('c: '))
+
+print(a, b, c)
+#berekenen
+
+discriminant = b**2 - (4 * a * c)
+
+print(discriminant)
