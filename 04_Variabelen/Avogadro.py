71fe28f954209289c171c598a94181eb54ed7c7f
ArthurCallewaert <arthur.callewaert@sgsintpaulus.eu>
2019-09-26 16:00:00 +0200

    coulumb

---
+++ 04_Variabelen/Avogadro.py
@@ -0,0 +1,13 @@
+#invoer
+mol = float(input('aantal mol: '))
+Na = 6.020 * 10 **23
+M = 32.06
+
+
+#berekening
+aantal_deeltjes = mol * Na
+massa = M * mol
+
+#uitvoer
+print(massa)
+print(aantal_deeltjes)
